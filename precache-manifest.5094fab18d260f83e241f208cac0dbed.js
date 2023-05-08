a51caaee3f2d4115cb2625b9eea528f3300161f6
Your Name <you@example.com>
2023-05-08 19:46:54 +0700

    first deployment

---
+++ precache-manifest.5094fab18d260f83e241f208cac0dbed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d25cd840317c44902e23dcaf6be4df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d0b0909e2c192409d9",
+    "url": "/static/css/2.3d433e25.chunk.css"
+  },
+  {
+    "revision": "41fd8a4a0881d73777d1",
+    "url": "/static/css/main.bf113eb6.chunk.css"
+  },
+  {
+    "revision": "e8d0b0909e2c192409d9",
+    "url": "/static/js/2.7dace82d.chunk.js"
+  },
+  {
+    "revision": "d574ea148687c7831aa7f45d136c98d8",
+    "url": "/static/js/2.7dace82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41fd8a4a0881d73777d1",
+    "url": "/static/js/main.9a02cd5e.chunk.js"
+  },
+  {
+    "revision": "3f8b7f4c04e766f3b98e",
+    "url": "/static/js/runtime-main.3486abd2.js"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/FontAwesome.b06871f2.ttf"
+  }
+]);
